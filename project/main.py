d6cfd13bae5156673135dd1a93b3a6a5c24c79bd
ineslidorio12 <ineslidoorio@gmail.com>
2024-12-29 23:38:12 +0000

    finalização do menu inicial em termos de interface

--- project/main.py
+++ project/main.py
@@ -9,15 +9,17 @@ ALTURA_JANELA = 800
 JANELA = pygame.display.set_mode((LARGURA_JANELA, ALTURA_JANELA))
 pygame.display.set_caption("Tradutor Gestual")
 
+IMAGEM_FUNDO = pygame.image.load("project/assets/fundo.png")
+IMAGEM_FUNDO = pygame.transform.scale(IMAGEM_FUNDO, (LARGURA_JANELA, ALTURA_JANELA))
 
 BRANCO = (255, 255, 255)
 PRETO = (0, 0, 0)
 AZUL = (0, 122, 255)
-CINZENTO = (79, 79, 79)
+CINZENTO = (79, 79, 79) 
 ROSA = (219, 112, 147)
 
 FONTE_TITULO = pygame.font.Font(None, 74)
-FONTE_BOTAO = pygame.font.Font(None, 74)
+FONTE_BOTAO = pygame.font.Font("project/utils/dogicapixel.ttf", 18)
 
 def desenhar_texto(janela, texto, fonte, cor, posicao):
     texto_superficie = fonte.render(texto, True, cor)
@@ -25,24 +27,42 @@ def desenhar_texto(janela, texto, fonte, cor, posicao):
     janela.blit(texto_superficie, texto_retangulo)
 
 
+def texto_clicado(texto, fonte, posicao, event):
+    texto_superficie = fonte.render(texto, True, BRANCO)
+    texto_retangulo = texto_superficie.get_rect(center=posicao)
+    
+    if event.type == pygame.MOUSEBUTTONDOWN and texto_retangulo.collidepoint(event.pos):
+        return True
+    return False
+
+def nova_janela(titulo):
+    while True:
+        JANELA.fill(PRETO)
+        desenhar_texto(JANELA, titulo, FONTE_TITULO, PRETO, (LARGURA_JANELA // 2, ALTURA_JANELA // 2))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                return
+        
+        pygame.display.flip()
+
+# menu inicial ----------------------------------------------------------------
 def menu_principal():
     while True:
-        JANELA.fill(BRANCO)
+        JANELA.blit(IMAGEM_FUNDO, (0, 0))
         
-        desenhar_texto(JANELA, "Tradutor Gestual", FONTE_TITULO, PRETO, (LARGURA_JANELA // 2, 100))
+        # desenhar_texto(JANELA, "Tradutor Gestual", FONTE_TITULO, BRANCO, (LARGURA_JANELA // 2, 100))
 
-        espaco_vertical = 100
-        botao_aprender = pygame.Rect(LARGURA_JANELA // 2 - 150, 200, 300, 60)
-        botao_treino = pygame.Rect(LARGURA_JANELA // 2 - 150, 200 + espaco_vertical, 300, 60)
-        botao_desafio = pygame.Rect(LARGURA_JANELA // 2 - 150, 200 + 2 * espaco_vertical, 300, 60)
- 
-        pygame.draw.rect(JANELA, AZUL, botao_aprender)
-        pygame.draw.rect(JANELA, AZUL, botao_treino)
-        pygame.draw.rect(JANELA, AZUL, botao_desafio)
+        pos_aprender = (LARGURA_JANELA // 2, 350)
+        pos_treino = (LARGURA_JANELA // 2, 400)
+        pos_desafio = (LARGURA_JANELA // 2, 450)
         
-        desenhar_texto(JANELA, "Aprender", FONTE_BOTAO, BRANCO, botao_aprender.center)
-        desenhar_texto(JANELA, "Treino", FONTE_BOTAO, BRANCO, botao_treino.center)
-        desenhar_texto(JANELA, "Desafio", FONTE_BOTAO, BRANCO, botao_desafio.center)
+        desenhar_texto(JANELA, "APRENDER", FONTE_BOTAO, BRANCO, pos_aprender)
+        desenhar_texto(JANELA, "TREINO", FONTE_BOTAO, BRANCO, pos_treino)
+        desenhar_texto(JANELA, "DESAFIO", FONTE_BOTAO, BRANCO, pos_desafio)
     
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -50,12 +70,12 @@ def menu_principal():
                 sys.exit()
             
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if botao_aprender.collidepoint(event.pos):
-                    print("Aprender Clicado")
-                if botao_treino.collidepoint(event.pos):
-                    print("Treino Clicado")
-                if botao_desafio.collidepoint(event.pos):
-                    print("Desafio Clicado")
+                if texto_clicado("Aprender", FONTE_BOTAO, pos_aprender, event):
+                    nova_janela("Modo Aprender")
+                if texto_clicado("Treino", FONTE_BOTAO, pos_treino, event):
+                    nova_janela("Modo Treino")
+                if texto_clicado("Desafio", FONTE_BOTAO, pos_desafio, event):
+                    nova_janela("Modo Desafio")
                     
         pygame.display.flip()
 
