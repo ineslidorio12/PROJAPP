807ea5bac30e1cf8a9218c0520dd70fc7f170ba8
ineslidorio12 <ineslidoorio@gmail.com>
2024-12-29 19:42:35 +0000

    inclusao do modo treino na pagina inicial

---
+++ project/screens/treino.py
@@ -0,0 +1,13 @@
+from utils.text import draw_text
+from utils.buttons import draw_button
+
+def treino(screen, background_color):
+    """Renderiza a tela do modo Treino"""
+    screen.fill(background_color)
+    draw_text(screen, "Modo Treino", 400, 300, size=50)
+
+    # Botão para voltar ao menu
+    if draw_button(screen, "VOLTAR", 300, 500, 200, 50):
+        return "menu"
+
+    return "treino"
