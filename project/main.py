d389b0c33eeb888179749e1011b58e50756fcf97
ineslidorio12 <ineslidoorio@gmail.com>
2025-01-02 19:21:39 +0000

    adição da palavra e pagina inicial ao menu

--- project/main.py
+++ project/main.py
@@ -3,7 +3,7 @@ import sys
 
 pygame.init()
 
-# janela -----------------------------------
+# ediçoes basicas -----------------------------------
 LARGURA_JANELA = 800
 ALTURA_JANELA = 800
 JANELA = pygame.display.set_mode((LARGURA_JANELA, ALTURA_JANELA))
@@ -18,6 +18,8 @@ CINZENTO = (79, 79, 79)
 
 FONTE_TITULO = pygame.font.Font(None, 74)
 FONTE_BOTAO = pygame.font.Font("project/utils/dogicapixel.ttf", 18)
+
+# definicao da app ---------------------------------------
 
 def desenhar_texto(janela, texto, fonte, cor, posicao):
     texto_superficie = fonte.render(texto, True, cor)
@@ -46,7 +48,9 @@ def nova_janela(titulo):
                 return
         
         pygame.display.flip()
+        
 # pagina principal ------------------------------------------------------------
+
 def pagina_inicial():
     while True:
         JANELA.blit(IMAGEM_FUNDO, (0, 0))
@@ -62,19 +66,20 @@ def pagina_inicial():
         desenhar_texto(JANELA, "JOGAR", FONTE_BOTAO, cor_jogar, pos_jogar)
         desenhar_texto(JANELA, "SAIR", FONTE_BOTAO, cor_sair, pos_sair)
         
-        for evento in pygame.event.get():
-            if evento.type == pygame.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            if texto_clicado("JOGAR", FONTE_BOTAO, pos_jogar, evento):
+            if texto_clicado("JOGAR", FONTE_BOTAO, pos_jogar, event):
                 menu_principal()
-            if texto_clicado("SAIR", FONTE_BOTAO, pos_sair, evento):
+            if texto_clicado("SAIR", FONTE_BOTAO, pos_sair, event):
                 pygame.quit()
                 sys.exit()
 
         pygame.display.flip()
         
 # menu inicial ----------------------------------------------------------------
+
 def menu_principal():
     while True:
         JANELA.blit(IMAGEM_FUNDO, (0, 0))
@@ -82,18 +87,21 @@ def menu_principal():
         pos_aprender = (LARGURA_JANELA // 2, 300)
         pos_treino = (LARGURA_JANELA // 2, 350)
         pos_desafio = (LARGURA_JANELA // 2, 400)
-        pos_sair = (LARGURA_JANELA // 2, 450)
+        pos_inicial = (LARGURA_JANELA // 2, 450)
+        pos_sair = (LARGURA_JANELA // 2, 500)
         
         mouse_pos = pygame.mouse.get_pos()
         
         cor_aprender = CINZENTO if desenhar_texto(JANELA, "APRENDER", FONTE_BOTAO, BRANCO, pos_aprender).collidepoint(mouse_pos) else BRANCO
         cor_treino = CINZENTO if desenhar_texto(JANELA, "TREINO", FONTE_BOTAO, BRANCO, pos_treino).collidepoint(mouse_pos) else BRANCO
         cor_desafio = CINZENTO if desenhar_texto(JANELA, "DESAFIO", FONTE_BOTAO, BRANCO, pos_desafio).collidepoint(mouse_pos) else BRANCO
+        cor_inicial = CINZENTO if desenhar_texto(JANELA, "PAGINA INICIAL", FONTE_BOTAO, BRANCO, pos_inicial).collidepoint(mouse_pos) else BRANCO
         cor_sair = CINZENTO if desenhar_texto(JANELA, "SAIR", FONTE_BOTAO, BRANCO, pos_sair).collidepoint(mouse_pos) else BRANCO
 
         desenhar_texto(JANELA, "APRENDER", FONTE_BOTAO, cor_aprender, pos_aprender)
         desenhar_texto(JANELA, "TREINO", FONTE_BOTAO, cor_treino, pos_treino)
         desenhar_texto(JANELA, "DESAFIO", FONTE_BOTAO, cor_desafio, pos_desafio)
+        desenhar_texto(JANELA, "PAGINA INICIAL", FONTE_BOTAO, cor_inicial, pos_inicial)
         desenhar_texto(JANELA, "SAIR", FONTE_BOTAO, cor_sair, pos_sair)
                 
         for event in pygame.event.get():
@@ -101,12 +109,14 @@ def menu_principal():
                 pygame.quit()
                 sys.exit()
             
-            if texto_clicado("Aprender", FONTE_BOTAO, pos_aprender, event):
+            if texto_clicado("APRENDER", FONTE_BOTAO, pos_aprender, event):
                 nova_janela("Modo Aprender")
-            if texto_clicado("Treino", FONTE_BOTAO, pos_treino, event):
+            if texto_clicado("TREINO", FONTE_BOTAO, pos_treino, event):
                 nova_janela("Modo Treino")
-            if texto_clicado("Desafio", FONTE_BOTAO, pos_desafio, event):
+            if texto_clicado("DESAFIO", FONTE_BOTAO, pos_desafio, event):
                 nova_janela("Modo Desafio")
+            if texto_clicado("PAGINA INICIAL", FONTE_BOTAO, pos_inicial, event):
+                return
             if texto_clicado("SAIR", FONTE_BOTAO, pos_sair, event):
                 pygame.quit()
                 sys.exit()
